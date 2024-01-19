5999c69bddfecca4a5aba4bb2c982421cc5c6c37
rossi <gustavodevrr@gmail.com>
2024-01-18 23:01:24 -0300

    implementa funcionalidade de save slots
    Utiliza localstorage para salvar dados

--- index.js
+++ index.js
@@ -30,15 +30,8 @@ function loadState(state) {
 // state collecting ================
 
 inputs.forEach(input => input?.addEventListener('change', () => {
-  const rawData = collectInputState();
-  const isEmpty = Object.keys(rawData).length == 0;
-  const encodedData = isEmpty ? '' : btoa(JSON.stringify(rawData));
-
-  const newUrl = new URL(window.location.href); 
-  const newUrlParams = new URLSearchParams(window.location.search);
-  newUrlParams.set('state', encodedData);
-  newUrl.search = newUrlParams;
-  window.history.pushState(rawData, undefined, newUrl);
+  const state = collectInputState();
+  pushState(state);
 }));
 
 function collectInputState() {
@@ -49,12 +42,74 @@ function collectInputState() {
       if (val) acc[input.id] = val; 
       return acc;
     }, {}
-  );
+    );
 
   return rawData;
 }
 
+// save slots ================
+
+const saveSlotsContainer = document.querySelector('#save-slots');
+const saveSlots = saveSlotsContainer.querySelectorAll('[data-save-slot]')
+let selectedSlot = null;
+
+
+saveSlots
+.forEach(saveSlot => {
+  const saveSlotId = saveSlot.dataset.saveSlot;
+  if (!saveSlotId) return;
+
+  const saveButton = saveSlot.querySelector('.save');
+  saveButton.addEventListener('click', () => {
+    const state = collectInputState();
+    alert(`Saving slot ${saveSlotId}`);
+    window.localStorage.setItem(saveSlotId, JSON.stringify(state));
+  });
+
+  const loadButton = saveSlot.querySelector('.load');
+  loadButton.addEventListener('click', () => {
+    const saveSlotData = window.localStorage.getItem(saveSlotId);
+    if (!saveSlotData) {
+      alert(`No save in slot ${saveSlotId}`);
+      return;
+    }
+    alert(`Loading slot ${saveSlotId}`);
+    const saveSlotState = JSON.parse(saveSlotData);
+    selectSlot(saveSlot);
+
+    loadState(saveSlotState);
+    pushState(saveSlotState);
+  });
+
+  const deleteButton = saveSlot.querySelector('.delete');
+  deleteButton.addEventListener('click', () => {
+    alert(`Deleting slot ${saveSlotId}`);
+    selectSlot(null);
+    window.localStorage.removeItem(saveSlotId);
+  });
+})
+
+function selectSlot(slot) {
+  selectedSlot?.classList.remove('selected');
+  selectedSlot = slot;
+  selectedSlot.classList.add('selected');
+
+  const saveSlotId = slot.dataset.saveSlot;
+  if (saveSlotId == null) return;
+}
+
 // utils ================
+
+function pushState(state) {
+  const isEmpty = Object.keys(state).length == 0;
+  const encodedData = isEmpty ? '' : btoa(JSON.stringify(state));
+
+  const newUrl = new URL(window.location.href); 
+  const newUrlParams = new URLSearchParams(window.location.search);
+  newUrlParams.set('state', encodedData);
+  newUrl.search = newUrlParams;
+  window.history.pushState(state, undefined, newUrl);
+}
 
 function getInputVal(el) {
   return !isCheckbox(el) ? el.value : el.checked;
